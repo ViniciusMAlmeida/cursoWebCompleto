a7c22e7a605690fa3ff5277dfe0c2d4e01cf28eb
vinicius.almeida <vinicius.almeida@coopertec.com.br>
2019-05-29 18:43:40 -0300

    até a aula 21

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -1,6 +1,8 @@
-console.log(7 / 0)
-console.log("10" / 2)
-console.log("show" * 2)
-console.log(0.1 + 0.7)
-//console.log(10.toSring())
-console.log((10.324).toString())
+console.log(7 / 0) // retorna Infinity
+console.log("10" / 2) //efetua a divisão
+console.log('3' + 2) //String ganha! Como soma(sinal de +) faz parte do contexto string, será efetuado a concatenação ao invés da soma
+console.log('3' - 2) //subtração não faz parte do contexto de string, logo será efetuado a subtração numérica ao invés da concatenação
+console.log("show" * 2) //retorna um NaN (Not a Number)
+console.log(0.1 + 0.7) //soma aproximada, conforme padrão da IEEE
+//console.log(10.toSring()) // é lançado uma excessão, pois 10 não pode ser convertido em um String
+console.log((10.324).toString()) //através dos parênteses () é edetuado um parse para o tipo Number, e Number pode ser convertido em String
