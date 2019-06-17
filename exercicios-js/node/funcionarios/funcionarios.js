8e17ff17f768e85d8997ed71cb3e34a947702111
vinicius.almeida <vinicius.almeida@coopertec.com.br>
2019-06-17 17:05:50 -0300

    aula 127 - Resolvendo Desafio

--- exercicios-js/node/funcionarios/funcionarios.js
+++ exercicios-js/node/funcionarios/funcionarios.js
@@ -1,7 +1,17 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+const mulheres = funcionario => funcionario.genero === 'F'
+const chineses = funcionario => funcionario.pais === 'China'
+const menorSalario = (func, funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
+
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+    //console.log(funcionarios)
+
+    // mulher chinesa com menor salario?
+    const func = funcionarios.filter(mulheres).filter(chineses).reduce(menorSalario)
+    console.log(func)
 })
